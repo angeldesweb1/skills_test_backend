52f2470021bffe1a8dae6cd287c06ee7f743df77
angeldesweb <angeldesweb@gmail.com>
2026-01-27 00:30:28 -0400

    fix: :bug: fixes http adapter implementation error

--- src/lib/adapters/http/http.adapter.ts
+++ src/lib/adapters/http/http.adapter.ts
@@ -1,8 +1,15 @@
 import { CoreAdapter } from '@lib/interfaces';
 import { injectable } from 'inversify';
+import { SupportedAdapters } from '..';
 
 @injectable()
 export class HTTPAdapter implements CoreAdapter {
+  public name: SupportedAdapters = 'http';
+
+  configure(...args: unknown[]): unknown {
+    throw new Error('Method not implemented.');
+  }
+
   mount(...args: unknown[]): unknown {
     throw new Error('Method not implemented.');
   }
